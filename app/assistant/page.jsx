ca9ad47392be0799fc1be61d0d95f2abee27f5d0
dededimos <pdededimos@gmail.com>
2025-08-02 15:58:52 -0700

    Delete app/assistant/page.jsx

--- app/assistant/page.jsx
+++
@@ -1,9 +0,0 @@
-import AIActionLog from '../../components/AI/AIActionLog';
-
-export default function AssistantPage() {
-  return (
-    <main className="p-4">
-      <AIActionLog />
-    </main>
-  );
-}
