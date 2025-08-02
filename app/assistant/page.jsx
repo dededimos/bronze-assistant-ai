4ff24b1984befae0dde404c1056c47189b3a0ada
dededimos <pdededimos@gmail.com>
2025-08-02 16:00:00 -0700

    Create page.jsx

---
+++ app/assistant/page.jsx
@@ -0,0 +1,11 @@
+'use client';
+
+import AIActionLog from '../../components/AI/AIActionLog';
+
+export default function AssistantPage() {
+  return (
+    <main className="p-4">
+      <AIActionLog />
+    </main>
+  );
+}
