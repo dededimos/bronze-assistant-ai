51b6b3c332d53ea98800a6cb88b5cd07b3a5505b
dededimos <pdededimos@gmail.com>
2025-08-03 21:13:56 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-
-
-import AIActionLog from "../components/AI/AIActionLog";
+import ChatInterface from "../components/AI/ChatInterface";
 
 export default function Home() {
   return (
-    <main className="p-6">
-      <AIActionLog />
+    <main className="p-6 space-y-6">
+      <ChatInterface />
     </main>
   );
 }
