ef16cacc1b471d45d070e4445edd9352affff0c9
dededimos <pdededimos@gmail.com>
2025-08-02 18:09:02 -0700

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import AIActionLog from "../components/AI/AIActionLog.jsx";
+
+export default function Home() {
+  return (
+    <main className="p-4">
+      <AIActionLog />
+    </main>
+  );
+}
